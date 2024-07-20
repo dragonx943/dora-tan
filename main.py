bae51b1447c915a68fb5ba20db91456d3ddaee83
Draken <131387159+dragonx943@users.noreply.github.com>
2024-07-20 11:09:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ from discord.ext import commands
 
 api_id = 
 api_hash = ''
-client = TelegramClient('', api_id, api_hash)
+telegram_client = TelegramClient('', api_id, api_hash)
 telegram_channel = '' # Ví dụ: https://t.me/douban_read
 
 discord_token = ''
 discord_thread_id = ''
-
-telegram_client = TelegramClient('tele_tips', api_id, api_hash)
 
 intents = discord.Intents.default()
 intents.message_content = True
