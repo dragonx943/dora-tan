8caa1c8b8cc7d58eec97bc590642b79b6a73bd0d
Draken <131387159+dragonx943@users.noreply.github.com>
2024-07-23 16:43:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ import discord
 from discord.ext import commands
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.video.io.VideoFileClip import VideoFileClip
-import datetime
-import pytz
 
 api_id = 
 api_hash = ''
@@ -32,7 +30,7 @@ logging.basicConfig(
 async def send_file_to_discord(file_path, thread):
     await thread.send(file=discord.File(file_path))
 
-def split_video(file_path, segment_duration=120):
+def split_video(file_path, segment_duration=60):
     video = VideoFileClip(file_path)
     total_duration = int(video.duration)
     current_start = 0
@@ -46,23 +44,23 @@ def split_video(file_path, segment_duration=120):
         try:
             ffmpeg_extract_subclip(file_path, current_start, current_end, targetname=part_path)
             parts.append(part_path)
-            print(f"Split part {part_index} from {current_start} to {current_end}")
+            print(f"Đang chia thành đoạn thứ {part_index} từ phân cảnh {current_start} đến {current_end}")
             current_start = current_end
             part_index += 1
         except Exception as e:
-            print(f"Exception during splitting: {e}")
+            print(f"Đã xảy ra lỗi khi tách file video: {e}")
             break
 
     video.close()
     return parts
 
-async def download_file(media, filename, retries=5):
+async def download_file(media, filename, retries=10):
     for attempt in range(retries):
         try:
             await telegram_client.download_media(media, filename)
             return filename
         except TimeoutError:
-            print(f"TimeoutError: Retry {attempt + 1}/{retries}")
+            print(f"Lỗi TimeOut: Thử lại lần thứ {attempt + 1}/{retries}")
             if attempt + 1 == retries:
                 raise
             await asyncio.sleep(5)
@@ -91,12 +89,16 @@ async def download_and_send_messages(thread, telegram_channel):
         await thread.send("Lỗi khi tham gia vào nhóm / kênh Telegram. Xin hãy cung cấp 1 Link lời mời hợp lệ!")
         return
 
+    invite_id = telegram_channel.split('/')[-1]
+    work_dir = f'./telegram_{invite_id}'
+    os.makedirs(work_dir, exist_ok=True)
+
     async with telegram_client:
         print('=== Bắt đầu tải hình ảnh từ Telegram! ===')
         photos = await telegram_client.get_messages(telegram_channel, None, filter=InputMessagesFilterPhotos)
         total_photos = len(photos)
         for index, photo in enumerate(photos, start=1):
-            filename = f"./telegram/{photo.id}.jpg"
+            filename = f"{work_dir}/{photo.id}.jpg"
             print(f"Đang tải: {index} / {total_photos} ảnh | Tên tệp: {filename}")
             await download_file(photo, filename)
             await send_file_to_discord(filename, thread)
@@ -108,8 +110,8 @@ async def download_and_send_messages(thread, telegram_channel):
             file_name = attributes[0].file_name if len(attributes) == 1 else attributes[1].file_name
             print(f"Đang tải tệp: {file_name}")
             try:
-                await download_file(file, f"./telegram/{file_name}")
-                await send_file_to_discord(f"./telegram/{file_name}", thread)
+                await download_file(file, f"{work_dir}/{file_name}")
+                await send_file_to_discord(f"{work_dir}/{file_name}", thread)
             except Exception as e:
                 print(f"Đã xảy ra lỗi khi tải về file: {file_name}: {e}")
         
@@ -117,7 +119,7 @@ async def download_and_send_messages(thread, telegram_channel):
         videos = await telegram_client.get_messages(telegram_channel, None, filter=InputMessagesFilterVideo)
         total_videos = len(videos)
         for index, video in enumerate(videos, start=1):
-            filename = f"./telegram/{video.id}.mp4"
+            filename = f"{work_dir}/{video.id}.mp4"
             print(f"Đang tải về: {index} / {total_videos} videos | Tên tệp: {filename}")
             try:
                 await download_file(video, filename)
@@ -131,37 +133,27 @@ async def download_and_send_messages(thread, telegram_channel):
             except Exception as e:
                 print(f"Đã xảy ra lỗi khi tải: {filename}: {e}")
 
-    # Xóa thư mục "telegram" sau khi hoàn tất
-    if os.path.exists('./telegram/'):
-        shutil.rmtree('./telegram/')
-        print('Đã xóa thư mục "telegram"')
+    shutil.rmtree(work_dir)
+    print(f'Đã xóa thư mục "{work_dir}"')
 
 @bot.slash_command(description="Tải nội dung từ Telegram và gửi vào chủ đề Discord")
-async def crawl(ctx, discord_thread_id: discord.Option(str, description="Nhập ID chủ đề Discord"), telegram_channel: discord.Option(str, description="Nhập Link từ Telegram")):
+async def crawl(ctx, discord_thread_id: discord.Option(str, description="Nhập ID chủ đề Discord vào đây!"), telegram_channel: discord.Option(str, description="Nhập Link mời từ Telegram vào đây!")):
     await ctx.defer()
     try:
-        await ctx.send_followup(f"Bắt đầu tải từ {telegram_channel} vào chủ đề {discord_thread_id}")
+        await ctx.send_followup(f"**<a:sip:1149986505964662815> Bắt đầu tải dữ liệu từ `{telegram_channel}` vào chủ đề <#{discord_thread_id}>**")
         thread = bot.get_channel(int(discord_thread_id))
         if thread is None:
-            await ctx.send_followup(f'Không thể tìm thấy chủ đề với ID: {discord_thread_id} trên Discord!')
+            await ctx.send_followup(f'**<a:zerotwo:1149986532678189097> Không thể tìm thấy chủ đề với ID: `{discord_thread_id}` trên Discord!**')
         else:
             await download_and_send_messages(thread, telegram_channel)
-            await ctx.send_followup(f"Đã hoàn tất tải từ {telegram_channel} vào chủ đề {discord_thread_id}")
+            await ctx.send_followup(f"**<a:emoji_anime:1149986363802918922> Đã hoàn tất tải từ `{telegram_channel}` vào chủ đề <#{discord_thread_id}>**")
     except Exception as e:
-        await ctx.send_followup(f"Đã xảy ra lỗi: {e}")
+        await ctx.send_followup(f"**<a:zerotwo:1149986532678189097> Đã xảy ra lỗi ngoài ý muốn: {e}**")
 
 @bot.event
 async def on_ready():
-
-    utc_offset = pytz.timezone('Asia/Ho_Chi_Minh')
-    current_time_utc7 = datetime.datetime.now(utc_offset).strftime("%H:%M - %d/%m")
-    utc = datetime.datetime.now(pytz.utc).strftime("%H:%M - %d/%m")
-    time = f"🌏 {current_time_utc7} GMT+7 | {utc} UTC 🌏"
-    activity=discord.Activity(type=discord.ActivityType.playing, name="Telegram Desktop", state=time)
+    activity=discord.Activity(type=discord.ActivityType.playing, name="Telegram Desktop", state="Bạn đọc dòng này làm gì? Bạn thích tôi à?")
     await bot.change_presence(status=discord.Status.dnd, activity=activity)
     print(f'Đã đăng nhập với Bot: {bot.user}')
-
-if not os.path.exists('./telegram/'):
-    os.makedirs('./telegram/')
 
 bot.run(discord_token)
